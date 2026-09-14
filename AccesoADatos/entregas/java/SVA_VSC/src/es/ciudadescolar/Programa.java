5646fb1dc7b1d65cf91fd68c30eecd9287b9df1c
JorgeVazquez <34370310+jorgevp2003@users.noreply.github.com>
2026-09-14 11:29:47 +0200

    Actualizacion

---
+++ AccesoADatos/entregas/java/SVA_VSC/src/es/ciudadescolar/Programa.java
@@ -0,0 +1,8 @@
+package es.ciudadescolar;
+public class Programa {
+    public static void main(String[] args) throws Exception {
+
+        //TODO: añadir siempre ficheros .log a las acciones
+        System.out.println("Empezamos Acceso a Datos…");
+    }
+}
