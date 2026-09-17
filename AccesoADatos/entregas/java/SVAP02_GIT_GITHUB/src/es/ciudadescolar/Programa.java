fc5416f5b73be24219a0a2b8767e4c3444d228ee
Jorge Vazquez <jorge.vazquez5@educa.madrid.org>
2026-09-17 13:06:53 +0200

    Creacion de MessageManager y muestra de mensaje a traves del mismo

--- AccesoADatos/entregas/java/SVAP02_GIT_GITHUB/src/es/ciudadescolar/Programa.java
+++ AccesoADatos/entregas/java/SVAP02_GIT_GITHUB/src/es/ciudadescolar/Programa.java
@@ -1,6 +1,11 @@
 package es.ciudadescolar;
+
+import es.ciudadescolar.utils.MessageManager;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Empezamos a usar Repositorios de Código ..");
+        MessageManager MM = new MessageManager();
+        System.out.println(MM.getTitulo()); 
+        
     }
 }
---
+++ AccesoADatos/entregas/java/SVAP02_GIT_GITHUB/src/es/ciudadescolar/utils/MessageManager.java
@@ -0,0 +1,8 @@
+package es.ciudadescolar.utils;
+
+public class MessageManager {
+    private final String titulo = "Empezamos a usar Repositorios de Código ...";
+
+    public String getTitulo(){return titulo;}
+    
+}
