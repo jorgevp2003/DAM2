f64878eeb78509e15e07afa3bdd72c6863a5fb07
Jorge Vazquez <jorge.vazquez5@educa.madrid.org>
2026-09-17 12:44:09 +0200

    Refactorizacion de clase: e.ciudadescolar.Programa

---
+++ AccesoADatos/entregas/java/SVAP02_GIT_GITHUB/src/es/ciudadescolar/Programa.java
@@ -0,0 +1,6 @@
+package es.ciudadescolar;
+public class Programa {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Empezamos a usar Repositorios de Código ..");
+    }
+}
