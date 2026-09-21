5a266462b59e6070d9e716a135d94a912323f279
Jorge Vazquez <34370310+jorgevp2003@users.noreply.github.com>
2026-09-21 10:35:28 +0200

    Borrado AADD
    Paso de AADD a una rama a parte

--- AccesoADatos/entregas/java/SVAP02_GIT_GITHUB/src/es/ciudadescolar/utils/MessageManager.java
+++
@@ -1,9 +0,0 @@
-package es.ciudadescolar.utils;
-
-public class MessageManager {
-    private final String titulo = "Empezamos a usar Repositorios de Código ...";
-
-     public String getTitulo(){return titulo;}
-    
-    
-}
--- AccesoADatos/entregas/java/SVA_VSC/src/es/ciudadescolar/Programa.java
+++
@@ -1,8 +0,0 @@
-package es.ciudadescolar;
-public class Programa {
-    public static void main(String[] args) throws Exception {
-
-        //TODO: añadir siempre ficheros .log a las acciones
-        System.out.println("Empezamos Acceso a Datos…");
-    }
-}
